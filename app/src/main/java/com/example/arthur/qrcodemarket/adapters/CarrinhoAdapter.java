504c90d2fcbf710dc7f7af652ea9b12cf57dfeed
Arthur <arthurguilher@gmail.com>
2015-11-28 23:13:25 -0300

    Reformulação do carrinho

--- app/src/main/java/com/example/arthur/qrcodemarket/adapters/CarrinhoAdapter.java
+++ app/src/main/java/com/example/arthur/qrcodemarket/adapters/CarrinhoAdapter.java
@@ -4,15 +4,13 @@ package com.example.arthur.qrcodemarket.adapters;
  * Created by Arthur on 26/10/2015.
  */
 
-import java.io.InputStream;
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,13 +18,15 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.arthur.qrcodemarket.CarrinhoActivity;
 import com.example.arthur.qrcodemarket.R;
 import com.example.arthur.qrcodemarket.entidades.Produto;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class CarrinhoAdapter extends ArrayAdapter<Produto> {
 
@@ -58,9 +58,10 @@ public class CarrinhoAdapter extends ArrayAdapter<Produto> {
             ProdutoWrapper.descricao = (TextView) item.findViewById(R.id.carrinho_subdescricao_produto);
             ProdutoWrapper.valor = (TextView) item.findViewById(R.id.carrinho_valor_produto);
             ProdutoWrapper.quantidade = (TextView) item.findViewById(R.id.carrinho_quantidade_produto);
-            ProdutoWrapper.botao_aumentar = (Button) item.findViewById(R.id.carrinho_botao_aumentar);
-            ProdutoWrapper.botao_diminuir = (Button) item.findViewById(R.id.carrinho_botao_diminuir);
-            ProdutoWrapper.botao_deletar = (Button) item.findViewById(R.id.carrinho_botao_deletar);
+            ProdutoWrapper.foto = (ImageView) item.findViewById(R.id.carrinho_foto_produto);
+            ProdutoWrapper.botao_aumentar = (ImageView) item.findViewById(R.id.carrinho_botao_aumentar);
+            ProdutoWrapper.botao_diminuir = (ImageView) item.findViewById(R.id.carrinho_botao_diminuir);
+            ProdutoWrapper.botao_deletar = (ImageView) item.findViewById(R.id.carrinho_botao_deletar);
             item.setTag(ProdutoWrapper);
         } else {
             ProdutoWrapper = (ProdutoWrapper) item.getTag();
@@ -69,10 +70,13 @@ public class CarrinhoAdapter extends ArrayAdapter<Produto> {
         final Produto produto = listaProdutos.get(position);
         ProdutoWrapper.name.setText(produto.getName());
         ProdutoWrapper.descricao.setText(produto.getDescricao());
-        ProdutoWrapper.valor.setText("Valor (unidade): R$: " + String.valueOf(produto.getValor()));
+        ProdutoWrapper.valor.setText("R$: " + String.valueOf(String.format("%.2f", produto.getValor())).replace(".", ","));
         ProdutoWrapper.quantidade.setText(String.valueOf(produto.getQuantidade()));
-        new DownloadImageTask((ImageView) item.findViewById(R.id.carrinho_foto_produto))
-                .execute(produto.getFoto());
+        if (produto.getFoto() != null || !produto.getFoto().equals("")) {
+            ProdutoWrapper.foto.setBackgroundColor(Color.TRANSPARENT);
+            new DownloadImageTask((ImageView) item.findViewById(R.id.carrinho_foto_produto))
+                    .execute(produto.getFoto());
+        }
         final CarrinhoAdapter.ProdutoWrapper finalProdutoWrapper = ProdutoWrapper;
 
         ProdutoWrapper.botao_aumentar.setOnClickListener(new OnClickListener() {
@@ -81,7 +85,7 @@ public class CarrinhoAdapter extends ArrayAdapter<Produto> {
             public void onClick(View v) {
 
                 //Toast.makeText(context, "Edit", Toast.LENGTH_LONG).show();
-                finalProdutoWrapper.quantidade.setText(String.valueOf(produto.getQuantidade()+1));
+                finalProdutoWrapper.quantidade.setText(String.valueOf(produto.getQuantidade() + 1));
                 produto.setQuantidade(produto.getQuantidade() + 1);
                 CarrinhoActivity.modificarValor(produto.getValor(), true);
             }
@@ -115,15 +119,33 @@ public class CarrinhoAdapter extends ArrayAdapter<Produto> {
 
     }
 
+    private void confirmationMessage(final int id, final Produto produto) { // Método para mostrar dialog de confirmação para pular etapa
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE: // Caso clique em sim
+                        listaProdutos.remove(id);
+                        notifyDataSetChanged();
+                        CarrinhoActivity.excluirValor(produto.getValor(), produto.getQuantidade());
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE: // Caso clique em não
+                        break;
+                }
+            }
+        };
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage("Tem certeza que deseja remover este produto do seu carrinho?").setPositiveButton("Sim", dialogClickListener).setNegativeButton("Não", dialogClickListener).show();
+    }
+
     static class ProdutoWrapper {
         TextView name;
         TextView valor;
         TextView quantidade;
         TextView descricao;
         ImageView foto;
-        Button botao_aumentar;
-        Button botao_diminuir;
-        Button botao_deletar;
+        ImageView botao_aumentar;
+        ImageView botao_diminuir;
+        ImageView botao_deletar;
     }
 
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
@@ -149,24 +171,6 @@ public class CarrinhoAdapter extends ArrayAdapter<Produto> {
         protected void onPostExecute(Bitmap result) {
             bmImage.setImageBitmap(result);
         }
-    }
-
-    private void confirmationMessage(final int id, final Produto produto) { // Método para mostrar dialog de confirmação para pular etapa
-        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case DialogInterface.BUTTON_POSITIVE: // Caso clique em sim
-                        listaProdutos.remove(id);
-                        notifyDataSetChanged();
-                        CarrinhoActivity.excluirValor(produto.getValor(), produto.getQuantidade());
-                        break;
-                    case DialogInterface.BUTTON_NEGATIVE: // Caso clique em não
-                        break;
-                }
-            }
-        };
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setMessage("Tem certeza que deseja remover este produto do seu carrinho?").setPositiveButton("Sim", dialogClickListener).setNegativeButton("Não", dialogClickListener).show();
     }
 
 }
